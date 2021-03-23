244cc3b43e4706755a5a260402df910d92c028ce
detb <benjamin.preece@hotmail.com>
2021-03-23 08:41:23 +0100

    adding maps

--- app/src/main/java/com/github/detb/trekkie/ui/home/RecentFragment.java
+++ app/src/main/java/com/github/detb/trekkie/ui/home/RecentFragment.java
@@ -4,24 +4,19 @@ import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.TextView;
 import android.widget.Toast;
 
 import androidx.annotation.NonNull;
-import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
-import androidx.lifecycle.Observer;
 import androidx.lifecycle.ViewModelProvider;
 import androidx.recyclerview.widget.DefaultItemAnimator;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
-import com.github.detb.trekkie.Hike;
 import com.github.detb.trekkie.HikeAdapter;
 import com.github.detb.trekkie.R;
 
 import java.util.ArrayList;
-import java.util.List;
 
 public class RecentFragment extends Fragment implements HikeAdapter.OnListItemClickListener{
 
@@ -38,6 +33,7 @@ public class RecentFragment extends Fragment implements HikeAdapter.OnListItemCl
         hikeList = root.findViewById(R.id.rv);
         hikeList.hasFixedSize();
         hikeList.setLayoutManager(new LinearLayoutManager(getActivity()));
+
 
 
         recentViewModel.getHikeLiveData().observe(getViewLifecycleOwner(), hikes -> {
--- app/src/main/java/com/github/detb/trekkie/ui/newroute/NewRouteFragment.java
+++ app/src/main/java/com/github/detb/trekkie/ui/newroute/NewRouteFragment.java
@@ -13,17 +13,33 @@ import androidx.lifecycle.Observer;
 import androidx.lifecycle.ViewModelProvider;
 
 import com.github.detb.trekkie.R;
+import com.mapbox.mapboxsdk.Mapbox;
+import com.mapbox.mapboxsdk.maps.MapView;
+import com.mapbox.mapboxsdk.maps.MapboxMap;
+import com.mapbox.mapboxsdk.maps.OnMapReadyCallback;
+import com.mapbox.mapboxsdk.maps.Style;
 
-public class NewRouteFragment extends Fragment {
+public class NewRouteFragment extends Fragment implements OnMapReadyCallback{
 
+    MapView mapView;
     private NewRouteViewModel newRouteViewModel;
 
     public View onCreateView(@NonNull LayoutInflater inflater,
                              ViewGroup container, Bundle savedInstanceState) {
+
+        Mapbox.getInstance(getContext(), getString(R.string.mapbox_access_token));
+
         newRouteViewModel =
                 new ViewModelProvider(this).get(NewRouteViewModel.class);
         View root = inflater.inflate(R.layout.fragment_newroute, container, false);
+
+
         final TextView textView = root.findViewById(R.id.text_gallery);
+
+        mapView = root.findViewById(R.id.mapView);
+        mapView.onCreate(savedInstanceState);
+        mapView.getMapAsync(this);
+
         newRouteViewModel.getText().observe(getViewLifecycleOwner(), new Observer<String>() {
             @Override
             public void onChanged(@Nullable String s) {
@@ -32,4 +48,57 @@ public class NewRouteFragment extends Fragment {
         });
         return root;
     }
+
+    @Override
+    public void onStart() {
+        super.onStart();
+        mapView.onStart();
+    }
+
+    @Override
+    public void onResume() {
+        super.onResume();
+        mapView.onResume();
+    }
+
+    @Override
+    public void onPause() {
+        super.onPause();
+        mapView.onPause();
+    }
+
+    @Override
+    public void onStop() {
+        super.onStop();
+        mapView.onStop();
+    }
+
+    @Override
+    public void onSaveInstanceState(Bundle outState) {
+        super.onSaveInstanceState(outState);
+        mapView.onSaveInstanceState(outState);
+    }
+
+    @Override
+    public void onLowMemory() {
+        super.onLowMemory();
+        mapView.onLowMemory();
+    }
+
+    @Override
+    public void onDestroy() {
+        super.onDestroy();
+        mapView.onDestroy();
+    }
+
+    @Override
+    public void onMapReady(@NonNull MapboxMap mapboxMap) {
+        mapboxMap.setStyle(Style.OUTDOORS, new Style.OnStyleLoaded() {
+            @Override
+            public void onStyleLoaded(@NonNull Style style) {
+                // Map is set up and the style has loaded. Now you can add data or make other map adjustments
+            }
+        });
+    }
+
 }
